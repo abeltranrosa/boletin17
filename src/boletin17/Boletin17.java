f0fca81ac1716b15e2edee323685dc4a31800e11
antonio <antonio@biruseto>
2017-02-01 09:00:40 +0100

    main boletin17a

---
+++ src/boletin17/Boletin17.java
@@ -0,0 +1,14 @@
+
+package boletin17;
+
+
+public class Boletin17 {
+
+    
+    public static void main(String[] args) {
+        NumerosAleatorios obx = new NumerosAleatorios();
+        obx.cargarArray();
+        obx.visualizarArray();
+    }
+    
+}
