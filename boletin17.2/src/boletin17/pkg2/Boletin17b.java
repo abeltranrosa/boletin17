e7973a7239f8857e88090bbb7ba9fffc306c4387
antonio <antonio@biruseto>
2017-02-01 09:01:35 +0100

    main boletin 17b

---
+++ boletin17.2/src/boletin17/pkg2/Boletin17b.java
@@ -0,0 +1,19 @@
+
+package boletin17.pkg2;
+
+
+public class Boletin17b {
+
+  
+    public static void main(String[] args) {
+        NotasProgramacion obx = new NotasProgramacion();
+        obx.visualizaAprobados();
+        obx.calcularNotaMedia();
+        obx.calcularNotaMaxima();
+        obx.cargarArray();
+        obx.pedirNotas();
+        
+        
+    }
+    
+}
