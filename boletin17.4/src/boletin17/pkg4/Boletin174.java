32098709aed207a20c9d6e805310af905be0ec10
antonio <antonio@biruseto>
2017-02-01 12:38:47 +0100

    main boletin 17.4

---
+++ boletin17.4/src/boletin17/pkg4/Boletin174.java
@@ -0,0 +1,15 @@
+
+package boletin17.pkg4;
+
+
+public class Boletin174 {
+
+   
+    public static void main(String[] args) {
+        LetraDni obx = new LetraDni();
+        obx.calcularLetra();
+        obx.pedirDNI();
+        obx.visualizar();
+    }
+    
+}
