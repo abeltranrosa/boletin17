a4e341fd5134133c007fe3d9c44dca6c95f670a1
antonio <antonio@biruseto>
2017-02-01 12:08:14 +0100

    main boletin 17.3

---
+++ Boletin17.3/src/boletin17/pkg3/Boletin173.java
@@ -0,0 +1,21 @@
+
+package boletin17.pkg3;
+
+
+public class Boletin173 {
+
+    
+    public static void main(String[] args) {
+        GrupoAlumnos obx = new GrupoAlumnos();
+        obx.cargarArray();
+        obx.listaNotas();
+        obx.visualizarAlumnoAprobado();
+        obx.consultarNotaAlumno();
+        obx.visualizaListaCompleta();
+        obx.visualizarAlumnoAprobado();
+    }
+}
+      
+    
+    
+
